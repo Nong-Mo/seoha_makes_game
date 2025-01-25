77cd04edd2e8a5d882f15adf0280d102014e45e2
ChoiJHy <choior12@gmail.com>
2025-01-25 15:16:41 +0900

    [Feat] 되감기 기능 틀 작성

--- Assets/Resources/Scripts/RewindableObject.cs
+++ Assets/Resources/Scripts/RewindableObject.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class RewindableObject : MonoBehaviour
 {
+    private bool is_rewinding = false;
+    private List<ObjectState> state_history = new List<ObjectState>();
+    public float rewind_duration = 3f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,20 @@ public class RewindableObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private struct ObjectState
+    {
+        public Vector3 position;
+        public Quaternion rotation;
+        public Vector3 velocity;
+
+        public ObjectState(Vector3 position, Quaternion rotation, Vector3 velocity)
+        {
+            this.position = position;
+            this.rotation = rotation;
+            this.velocity = velocity;
+        }
     }
 }
