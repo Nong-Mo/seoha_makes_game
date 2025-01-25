b587dc9c73e30c51083bd628d011b9e9f251f646
ChoiJHy <choior12@gmail.com>
2025-01-25 16:40:15 +0900

    [Feat] 되감기 매니저 기능 구

--- Assets/Resources/Scripts/RewindManager.cs
+++ Assets/Resources/Scripts/RewindManager.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class RewindManager : MonoBehaviour
 {
+    public List<RewindableObject> rewindable_objects = new List<RewindableObject>();
+    private bool is_rewinding = false;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
+    {   
+        // R키로 리와인드 시작
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            StartRewind();
+        }
+
+        // R키를 떼면 리와인드 종료
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            StopRewind();
+        }
+    }
+
+    public void StartRewind()
     {
-        
+        is_rewinding = true;
+        foreach(var obj in rewindable_objects)
+        {
+            obj.StartRewind();
+        }
+    }
+
+    public void StopRewind()
+    {
+        is_rewinding = false;
+        foreach (var obj in rewindable_objects)
+        {
+            obj.StopRewind();
+        }
     }
 }
