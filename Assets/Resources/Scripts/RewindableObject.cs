bcdab2cf2f9f0702abdb4c3a2c693e8594657b0f
ChoiJHy <choior12@gmail.com>
2025-01-25 21:07:17 +0900

    [Feat] 되감기 로직 수정

--- Assets/Resources/Scripts/RewindableObject.cs
+++ Assets/Resources/Scripts/RewindableObject.cs
@@ -6,9 +6,24 @@ public class RewindableObject : MonoBehaviour
 {
     private bool is_rewinding = false;
     private List<ObjectState> state_history = new List<ObjectState>();
+    private Rigidbody rb;
     public float rewind_duration = 3f;
 
-    private Rigidbody rb;
+    private struct ObjectState
+    {
+        public Vector3 position;
+        public Quaternion rotation;
+        public Vector3 velocity;
+        public bool is_active;
+
+        public ObjectState(Vector3 position, Quaternion rotation, Vector3 velocity, bool is_active)
+        {
+            this.position = position;
+            this.rotation = rotation;
+            this.velocity = velocity;
+            this.is_active = is_active;
+        }
+    }
 
     // Start is called before the first frame update
     private void Start()
@@ -27,50 +42,55 @@ public class RewindableObject : MonoBehaviour
     public void StartRewind()
     {
         is_rewinding = true;
-        rb.isKinematic = true; // 물리 작동 중지
+        rb.isKinematic = true; // 물리 비활성화
     }
 
     public void StopRewind()
     {
         is_rewinding = false;
         rb.isKinematic = false;
+
+        // 리와인드 종료 후 마지막 상태 복구
+        if (state_history.Count > 0)
+        {
+            ObjectState lastState = state_history[0];
+            rb.velocity = lastState.velocity;
+        }
+
     }
 
     private void RecordState()
     {
         if (state_history.Count > Mathf.Round(rewind_duration / Time.fixedDeltaTime))
-            state_history.RemoveAt(0); // 오래된 상태 삭제
+            state_history.RemoveAt(state_history.Count - 1);
 
-        state_history.Add(new ObjectState(transform.position, transform.rotation, rb.velocity));
+        state_history.Insert(0, new ObjectState(transform.position, transform.rotation, rb.velocity, gameObject.activeSelf));
+    }
+
+    public void Kill()
+    {
+        gameObject.SetActive(false); // 오브젝트 비활성화 
     }
 
     private void Rewind()
     {
-        if(state_history.Count > 0)
+        if (0 < state_history.Count)
         {
-            ObjectState last_state = state_history[state_history.Count - 1]; // 최근 상태 가져오기
-            transform.position = last_state.position;
-            transform.rotation = last_state.rotation;
-            rb.velocity = last_state.velocity;
-            state_history.RemoveAt(state_history.Count - 1); // 최근 상태 삭제
+            ObjectState state = state_history[0]; // 최근 상태 가져오기
+            transform.position = state.position;
+            transform.rotation = state.rotation;
+
+            // 죽은 오브젝트 복구
+            if (!state.is_active)
+            {
+                gameObject.SetActive(true);
+            }
+
+            state_history.RemoveAt(0); // 최근 상태 삭제
         }
         else
         {
             StopRewind();
-        }
-    }
-
-    private struct ObjectState
-    {
-        public Vector3 position;
-        public Quaternion rotation;
-        public Vector3 velocity;
-
-        public ObjectState(Vector3 position, Quaternion rotation, Vector3 velocity)
-        {
-            this.position = position;
-            this.rotation = rotation;
-            this.velocity = velocity;
         }
     }
 }
